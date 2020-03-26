fe38214141d72d927291b9e08779594f7c7cd3f6
Jim <jiaming7674@gmail.com>
2020-03-26 09:55:59 +0800

    Update awk_sample.sh

--- awk_sample.sh
+++ awk_sample.sh
@@ -5,3 +5,9 @@ cat /etc/passwd | awk 'BEGIN {FS=":"} {if ($1 ~ /^[a-z]{4}$/) {print $1}}'
 
 # 加總目前資料內的檔案大小
 ls -al | awk '{size+=$5} END {print "total size: " size}'
+
+# 分割字串 stbstr([input], [start position], [length]) (bash 陣列從1開始計算)
+cat test.hex | awk '{print substr($0, 4, 4)}'
+
+# 分割字串 split([input], [destination], [separator])
+ls -al | awk 'split($0, a, " "); print a[3]}'
